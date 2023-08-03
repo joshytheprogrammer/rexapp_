0ee6e71894adc8d9a13ac69fa78626e4e76c25ac
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-08-03 16:07:25 +0100

    made changes: 'adding templates'

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require("dotenv");
+const cookieParser = require('cookie-parser');
+
+dotenv.config();
 const app = express();
-const port = 3000; // Change this port number as per your preference
+
+app.use(cookieParser());
+
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:3000'],
+  optionsSuccessStatus: 200
+}));
+
+app.use(bodyParser.json());
+
+if(process.env.NODE_ENV == 'development') {
+  mongoose.connect(process.env.DEV_MONGO_URL)
+  .then(() => {
+    app.listen(process.env.PORT);
+  })
+  .catch((err) => {
+    console.log(err)
+  });
+}
+
+if(process.env.NODE_ENV == 'production') {
+
+  mongoose.connect(process.env.PROD_MONGO_URL)
+  .then(() => {
+    app.listen(process.env.PORT);
+  })
+  .catch((err) => {
+    console.log(err)
+  });
+
+}
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
@@ -11,9 +49,4 @@ app.set('views', __dirname + '/views');
 // Routes
 app.get('/', (req, res) => {
   res.render('index', { message: 'Hello, World!' });
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
