497e7127aeb637e09d51cbed6908fb5ec9a3cc76
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-08-08 14:45:17 +0100

    made changes: 'made seeder seed from files'

--- seeders/index.js
+++ seeders/index.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
 const Product = require('../models/Product');
 const Category = require('../models/Category');
 
@@ -10,52 +12,36 @@ mongoose.connect(process.env.DEV_MONGO_URL, {
   useUnifiedTopology: true,
 });
 
-// Sample categories and products data
-const categoriesData = [
-  { name: 'Engine Parts', slug: 'engine-parts', imageURL: 'engine-parts.jpg' },
-  { name: 'Brake System', slug: 'brake-system', imageURL: 'brake-system.jpg' },
-  // ... add more categories
-];
+// Read data from data.json
+const dataPath = path.join(__dirname, 'data.json');
+const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
 
-const productsData = [
-  {
-    name: 'Spark Plug',
-    category: 'Engine Parts',
-    manufacturer: 'AutoTech',
-    description: 'High-quality spark plug for efficient combustion.',
-    price: 9.99,
-    stock: 100,
-    imageURL: 'spark-plug.jpg',
-  },
-  {
-    name: 'Brake Pad',
-    category: 'Brake System',
-    manufacturer: 'BrakeMaster',
-    description: 'Durable brake pad for safe and effective braking.',
-    price: 29.99,
-    stock: 50,
-    imageURL: 'brake-pad.jpg',
-  },
-  // ... add more products
-];
-
-// Populate categories and products
-async function seedDatabase() {
+// Seed categories
+async function seedCategories() {
   try {
-    for (const categoryData of categoriesData) {
-      await Category.create(categoryData);
-    }
-
-    for (const productData of productsData) {
-      await Product.create(productData);
-    }
-
-    console.log('Database seeded successfully.');
+    await Category.insertMany(data.categories);
+    console.log('Categories seeded successfully.');
   } catch (error) {
-    console.error('Error seeding database:', error);
+    console.error('Error seeding categories:', error);
+  }
+}
+
+// Seed products
+async function seedProducts() {
+  try {
+    await Product.insertMany(data.products);
+    console.log('Products seeded successfully.');
+  } catch (error) {
+    console.error('Error seeding products:', error);
   } finally {
     mongoose.disconnect();
   }
+}
+
+// Seed database
+async function seedDatabase() {
+  await seedCategories();
+  await seedProducts();
 }
 
 // Run the seeding function
