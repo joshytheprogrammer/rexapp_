86e2ae348d5dc57253b24d6a549ce3c5b7b3b874
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-08-08 12:46:09 +0100

    made changes: added DB schema

---
+++ models/Product.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  category: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true },
+  imageURL: { type: String }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
